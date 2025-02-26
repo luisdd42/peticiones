5f1ede53d05fae24203614d57d1859d0c356c2b7
luis <duarte.luisdominguez@iesconcepcion.edu.ar>
2025-02-26 20:57:40 -0300

    componente condiciones

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Peticion } from "./componente/Peticion";
+import { Condicion } from "./componente/Condicion";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Peticion />
+      <Condicion edad={17} />
     </>
   );
 }
---
+++ src/componente/Condicion.jsx
@@ -0,0 +1,7 @@
+export const Condicion = ({ edad }) => {
+  if (edad <= 18) {
+    alert("es menor de edad");
+  } else {
+    alert("es mayor");
+  }
+};
